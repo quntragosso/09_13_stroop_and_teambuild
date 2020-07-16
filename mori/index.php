f20b1416ca47fa2ab2943bdc7daeb3b2b9688a3d
QunTragosso <karakara218@gmail.com>
2020-07-17 00:00:09 +0900

    second-commit

---
+++ mori/index.php
@@ -0,0 +1,204 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>なんちゃらニュースマガジン｜みんなのニュースを発信するWebメディア</title>
+    <link href="https://fonts.googleapis.com/earlyaccess/nicomoji.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <header>
+        <h1>なんちゃらニュースマガジン</h1>
+    </header>
+    <main>
+        <section id="main_topics">
+            <div class="l_2columns">
+                <div class="pickup_card_block" id="main_news">
+                    <div class="pickup_card">
+                        <div class="news_caps">
+                            <span class="news_date">2020.07.13</span>
+                            <span class="news_tag">カテゴリー</span>
+                        </div>
+                        <h2 class="news_ttl">注目ニュースのタイトル</h2>
+                        <p class="news_txt">ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約</p>
+                    </div>
+                    <div class="pickup_card comment">
+                        <h3 class="news_ttl">ニュースへのコメント</h3>
+                        <div class="news_caps">
+                            <span class="news_date">いいね 数　件 / </span>
+                            <span class="news_date">コメント数</span>
+                        </div>
+                        <dl>
+                            <dt>ユーザー名</dt>
+                            <dd>コメントコメントコメントコメント</dd>
+                            <dt>ユーザー名</dt>
+                            <dd>コメントコメントコメントコメント</dd>
+                            <dt>ユーザー名</dt>
+                            <dd>コメントコメントコメントコメント</dd>
+                            <dt>ユーザー名</dt>
+                            <dd>コメントコメントコメントコメント</dd>
+                        </dl>
+                    </div>
+                </div>
+            </div>
+            <div id="trend_latest_news" class="r_2columns">
+                <div class="news_list_block">
+                    <!-- tab -->
+                    <div class="tab_area">
+                        <input id="trend_news" type="radio" name="tab_btn" checked>
+                        <label class="tab_btn" for="trend_news">トレンドニュース</label>
+                        <input id="latest_news" type="radio" name="tab_btn">
+                        <label class="tab_btn" for="latest_news">最新のニュース</label>
+
+                        <div class="tab_content" id="trend_news_block">
+                            <h2>トレンドニュース</h2>
+                            <p>トレンドニュース説明文トレンドニュース説明文トレンドニュース説明文</p>
+                            <ul>
+                                <li>
+                                    <h3>トレンドニュース01のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>トレンドニュース02のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>トレンドニュース03のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>トレンドニュース04のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>トレンドニュース05のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <!--/trend_news_block-->
+
+                        <div class="tab_content" id="latest_news_block">
+                            <h2>最新のニュース</h2>
+                            <p>最新ニュース説明文最新ニュース説明文最新ニュース説明文</p>
+                            <ul>
+                                <li>
+                                    <h3>最新ニュース01のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>最新ニュース02のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>最新ニュース03のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>最新ニュース04のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                                <li>
+                                    <h3>最新ニュース05のタイトル</h3>
+                                    <div class="news_caps">
+                                        <span class="news_date">2020.07.13</span>
+                                        <span class="news_tag">カテゴリ名</span>
+                                        <span class="news_like">いいね数</span>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                        <!--/latest_news_block-->
+                    </div><!-- /tab -->
+                </div>
+                <!--/news_list_block-->
+        </section>
+        <!--/main_topics-->
+        <section id="editors_recommend">
+            <h2>編集部イチオシ</h2>
+            <p>編集部イチオシニュース説明文編集部イチオシニュース説明文</p>
+            <div class="pickup_card">
+                <div class="news_caps">
+                    <span class="news_date">2020.07.13</span>
+                    <span class="news_tag">カテゴリー</span>
+                </div>
+                <h3 class="news_ttl">注目ニュースのタイトル</h3>
+                <p class="news_txt">ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約</p>
+            </div>
+            <div class="pickup_card">
+                <div class="news_caps">
+                    <span class="news_date">2020.07.13</span>
+                    <span class="news_tag">カテゴリー</span>
+                </div>
+                <h3 class="news_ttl">注目ニュースのタイトル</h3>
+                <p class="news_txt">ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約</p>
+            </div>
+            <div class="pickup_card">
+                <div class="news_caps">
+                    <span class="news_date">2020.07.13</span>
+                    <span class="news_tag">カテゴリー</span>
+                </div>
+                <h3 class="news_ttl">注目ニュースのタイトル</h3>
+                <p class="news_txt">ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約ニューステキスト要約</p>
+            </div>
+        </section>
+        <section id="hot_categories">
+            <h2>今注目のカテゴリー</h2>
+            <div class="tag_lists">
+                <span><a href="#">カテゴリ名</a></span>
+                <span><a href="#">カテゴリ名</a></span>
+                <span><a href="#">カテゴリ名</a></span>
+                <span><a href="#">カテゴリ名</a></span>
+                <span><a href="#">カテゴリ名</a></span>
+                <span><a href="#">カテゴリ名</a></span>
+                <span><a href="#">カテゴリ名</a></span>
+                <span><a href="#">カテゴリ名</a></span>
+                <span><a href="#">カテゴリ名</a></span>
+                <span><a href="#">カテゴリ名</a></span>
+            </div>
+
+        </section>
+    </main>
+</body>
+
+</html>
